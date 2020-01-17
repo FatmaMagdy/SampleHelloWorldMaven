f3a0c437dfec08eeb47cc26006bfc929b8439ad3
Fatma Hammam <fhammam@cisco.com>
2020-01-17 13:45:10 -0800

    intial commit

---
+++ src/main/java/COM/fh/demo/App.java
@@ -0,0 +1,13 @@
+package COM.fh.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
